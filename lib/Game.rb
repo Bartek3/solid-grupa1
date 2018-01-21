0d9dcba1a6f9766fa886b490180e963f7cc176c6
pksskm <pavlo112@gmail.com>
2018-01-22 00:15:06 +0100

    Odseparowanie metod dla większej czytelności w pliku Game.rb

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,15 +1,15 @@
 require_relative 'Code'
 class Game
+  
 def initialize
 @secret_code = Code.new
-
 end
+
 def guess
 begin
 input = input_reader
 @code = Code.new(input)
 puts Code.info unless @code.valid?
-
 end while !(@code.valid?)
 if @secret_code.code == @code.code
 true
@@ -18,6 +18,7 @@ puts "#{@code} #{@secret_code.compare @code}"
 false
 end
 end
+
 def input_reader
 input = gets.chomp
 if input.include?("nowa")
@@ -27,14 +28,15 @@ puts "[to do] powinny sie wyswietlic ustawienia"
 end
 input
 end
+
 def play
 tries=0
 begin
 tries += 1
 end while not guess
 puts "Odgadłeś kod po #{tries} prób#{ tries == 1 ? "ie" : "ach"}."
-
 end
+
 def hello
 puts "","Mastermind 1.0".center(80)
 puts "długość_kodu = #{Code.length};"
