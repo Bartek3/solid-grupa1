6d9b593eb4f5f041c75b815d7f9b7522caf19c53
pksskm <pavlo112@gmail.com>
2018-01-22 12:11:12 +0100

    Game.rb - Zastosowanie pojedynczych zamiast podwójnych cudzysłowów w łańcuchach znaków bez interporacji

--- lib/Game.rb
+++ lib/Game.rb
@@ -21,10 +21,10 @@ end
 
 def input_reader
 input = gets.chomp
-if input.include?("nowa")
-puts "[to do] powinna sie zaczac nowa gra"
-elsif input.include?("ustawienia")
-puts "[to do] powinny sie wyswietlic ustawienia"
+if input.include?('nowa')
+puts '[to do] powinna sie zaczac nowa gra'
+elsif input.include?('ustawienia')
+puts '[to do] powinny sie wyswietlic ustawienia'
 end
 input
 end
@@ -34,18 +34,17 @@ tries=0
 begin
 tries += 1
 end while not guess
-puts "Odgadłeś kod po #{tries} prób#{ tries == 1 ? "ie" : "ach"}."
+puts "Odgadłeś kod po #{tries} prób#{ tries == 1 ? 'ie' : 'ach'}."
 end
 
 def hello
-puts "","Mastermind 1.0".center(80)
-puts "długość_kodu = #{Code.length};"
-puts "różnych_znaków = #{Code.complexity};",""
+puts '','Mastermind 1.0'.center(80)
+puts "długość_kodu = #{Code.length}"
+puts "różnych_znaków = #{Code.complexity}",''
 end
 
 end
 
-puts 'Hello'
 game = Game.new
 game.hello
 game.play
